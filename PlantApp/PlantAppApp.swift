ee6a9d8c7b663a152ba44a3fbfba2d935f242317
tuna2608 <tuna1.dev@gmail.com>
2023-04-26 15:01:46 +0700

    Initial Commit

---
+++ PlantApp/PlantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantAppApp.swift
+//  PlantApp
+//
+//  Created by Anh Tú on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
